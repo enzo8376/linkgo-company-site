b4bbce9705e9a2583a0fdfbd84cd6e2ee9269fbe
enzo8376 <enzo@iamgofer.com>
2026-08-06 17:05:20 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+/**
+ * 網站連結設定
+ * 發布前如需更換表單或 LINE，只要修改這個檔案即可。
+ */
+window.LINKGO_SITE_CONFIG = {
+  formUrl: 'https://lihi.cc/pVGI4',
+  lineUrl: 'https://lihi.cc/gBMqv',
+  lineId: '@gofer',
+  contactEmail: 'linkgo@iamgofer.com'
+};
